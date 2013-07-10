a10e5e5730860979260b61ab6389366ecce70d88
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2013-07-10 19:21:11 +0200

    Refactor javascript code.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -7,41 +7,51 @@
  * Based on the work from Izidor Matušov <izidor.matusov@gmail.com> (GTG Task Button)
  */
 
-/*
- * trims whitespace, reduces inner newlines and spaces
- */
-function trim(string, length) {
-    // get rid of stacked newlines
-    string = (string || '').replace(/\n{3,}/g, '\n\n');
 
-    // get rid of redonk spaces
-    string = string.replace(/\s{3,}/g, ' ');
-    string = string.replace(/^\s+/g,'').replace(/\s+$/g,'');
+(function(window) {
+    // Config options for this addon
+    var config = {
+        'addUrl' : 'https://www.wunderlist.com/#/extension/add/'
+    }
 
-    // only trim string length if it's
-    if (string.length > length) {
-        string = string.substring(0, length) + '...';
-    } 
+    // Get preferences for this addon
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                          .getService(Components.interfaces.nsIPrefService)
+                          .getBranch("extensions.wunderlist.");
 
-    return string;
-}
+    /*
+     * Trims whitespace, reduces inner newlines and spaces
+     */
+    function trim(string, length) {
+        // get rid of stacked newlines
+        string = (string || '').replace(/\n{3,}/g, '\n\n');
 
+        // get rid of redondant spaces
+        string = string.replace(/\s{3,}/g, ' ');
+        string = string.replace(/^\s+/g,'').replace(/\s+$/g,'');
 
-var wunderlist_thunderbird = {
+        // only trim string length if it's
+        if (string.length > length) 
+            string = string.substring(0, length) + '...';
+
+        return string;
+    }
+
     /*
      * Create a new task, using title, author, recipients and content
      */
-    createTask: function (event) {
+    function createTask(event) {
         var message = gFolderDisplay.selectedMessage;
         var messenger = Components.classes["@mozilla.org/messenger;1"]
-                                   .createInstance(Components.interfaces.nsIMessenger);
+                                  .createInstance(Components.interfaces.nsIMessenger);
         var listener = Components.classes["@mozilla.org/network/sync-stream-listener;1"]
-                                  .createInstance(Components.interfaces.nsISyncStreamListener);
+                                 .createInstance(Components.interfaces.nsISyncStreamListener);
 
-        var uri = message.folder.getUriForMsg(message);
-        messenger.messageServiceFromURI(uri).streamMessage(uri, listener, null, null, false, "");
         var folder = message.folder;
+        var uri = folder.getUriForMsg(message);
+        messenger.messageServiceFromURI(uri).streamMessage(uri, listener, null, null, false, "");
 
+        // Get title, author, recipients and message
         var title = trim(message.mime2DecodedSubject, 150) + " #mail";
         var author = "From: " + message.mime2DecodedAuthor + "\n";
         var recipients = "To: " + message.mime2DecodedRecipients + "\n\n";
@@ -51,26 +61,23 @@ var wunderlist_thunderbird = {
         title = encodeURIComponent(title);
         content = encodeURIComponent(author + recipients + trim(content, 5000));
 
+        // Open web page with Wunderlist add url
         var extProtocolSvc = Components.classes["@mozilla.org/uriloader/external-protocol-service;1"]
                                   .getService(Components.interfaces.nsIExternalProtocolService);
         var ioService = Components.classes["@mozilla.org/network/io-service;1"]
                              .getService(Components.interfaces.nsIIOService);
-        var wunderlisturi = ioService.newURI('https://www.wunderlist.com/#/extension/add/' 
+        var wunderlistUrl = ioService.newURI(config.addUrl 
                              + title + '/' + content, null, null);
-        extProtocolSvc.loadUrl(wunderlisturi);
+        extProtocolSvc.loadUrl(wunderlistUrl);
 
         RestoreFocusAfterHdrButton();
-    },
+    }
 
     /*
      * Add our button to toolbar during the first load
+     * The button will be on the right of archive
      */
-    initButton: function () {
-        // create object for accessing preferencies for this extension
-        var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-                            .getService(Components.interfaces.nsIPrefService)
-                            .getBranch("extensions.wunderlist.");
-                    
+    function initButton() {
         if (prefs.getBoolPref("firstRun")) {
             var button = 'wunderlist-button';
             var after = 'hdrArchiveButton';
@@ -80,7 +87,8 @@ var wunderlist_thunderbird = {
             pos = currentset.indexOf(after)
             if (after !== "" && pos >= 0) {
                 pos += after.length;
-                currentset = currentset.substr(0, pos) + "," + button + currentset.substr(pos, currentset.length);
+                currentset = currentset.substr(0, pos) + "," 
+                             + button + currentset.substr(pos, currentset.length);
             }
             else {
                 currentset = currentset + "," + button;
@@ -93,8 +101,11 @@ var wunderlist_thunderbird = {
         }
     }
 
-}
+    // add the button only when thunderbird is fully loaded
+    addEventListener('load', initButton, false);
 
-// add the button only when thunderbird is fully loaded
-addEventListener('load', wunderlist_thunderbird.initButton, false);
+    // exports
+    window.createTask = createTask;
+})(window)
+
 
